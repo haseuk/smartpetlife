d790f2ae0b0b4d7b73beb5a03831e0ba317683c2
haseuk <73511954+haseuk@users.noreply.github.com>
2022-08-05 16:09:29 +0900

    모바일 메뉴 오픈시 바디 스크롤 막기

--- shop/js/header.js
+++ shop/js/header.js
@@ -1,13 +1,17 @@
 let header = document.querySelector('header');
 let menu = document.querySelector('.menu');
 let dim = document.querySelector('.dim');
+let body = document.querySelector('body');
 menu.addEventListener('click', function () {
   if (header.classList.contains('open')) {
     header.classList.remove('open');
+    body.style.overflow = '';
   } else {
     header.classList.add('open');
+    body.style.overflow = 'hidden';
   }
 });
 dim.addEventListener('click', function() {
   header.classList.remove('open');
+  body.style.overflow = '';
 })
